901dd85ee1fc2c4ff083eb3cf8074b6a01236ad9
m-linde <linde.melissa34@gmail.com>
2022-01-06 22:37:00 +0100

    feat(Project): implement highlight post feature

--- src/App.vue
+++ src/App.vue
@@ -7,66 +7,92 @@
     />
 
     <article class="grid grid-cols-2 gap-10">
-      <v-blog-card />
-
-      <section>
-        <img
-          class="object-cover h-full w-full"
-          style="height: 300px"
-          src="./assets/images/surf.jpeg"
-          alt="Sport"
-        />
-
-        <div class="container max-w-xl flex justify-between py-3 text-xs">
-          <p class="text-black bg-white p-2 rounded-full w-18 text-center">
-            Welness
-          </p>
-          <p class="text-gray-400">
-            Publiée le 6 janvier 2022 par Jeremy Tellier
-          </p>
-        </div>
-
-        <div>
-          <p class="text-lg font-medium">
-            Rétention d'eau ? La solution, boire de l'eau !
-          </p>
-          <p>
-            L'importance de donner à son organisme la quantité d'eau suffisante
-            au quotidien, afin d'évitez la rétention d'eau.
-          </p>
-        </div>
-      </section>
+      <v-blog-card
+        :promote="promotedBlogPost.promote"
+        :image="promotedBlogPost.image"
+        :category="promotedBlogPost.category"
+        :publication="promotedBlogPost.publication"
+        :paragraph="promotedBlogPost.paragraph"
+        :description="promotedBlogPost.description"
+      />
     </article>
 
     <v-header title="Hello" subtitle="Lorem ipsum" />
 
     <article class="grid grid-cols-2 gap-10">
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
-
-      <v-blog-card />
+      <v-blog-card
+        v-for="blog in promotedBlogPostFalse"
+        :key="blog.paragraph"
+        :image="blog.image"
+        :category="blog.category"
+        :publication="blog.publication"
+        :paragraph="blog.paragraph"
+        :description="blog.description"
+      />
     </article>
   </main>
 </template>
 
 <script>
-import VHeader from '@/assets/VHeader'
-import VBlogCard from '@/assets/VBlogCard'
+import VHeader from '@/components/VHeader'
+import VBlogCard from '@/components/VBlogCard'
 
 export default {
   name: 'App',
 
   components: { VHeader, VBlogCard },
+
+  data: () => ({
+    blogs: [
+      {
+        promote: true,
+        image: require(`@/assets/images/sport.jpeg`),
+        category: 'Sport',
+        publication: 'Publiée le 6 janvier 2022 par Jeremy Tellier',
+        paragraph: 'Je veux être fort !',
+        description:
+          'Article sur mon point de vue concernant le gain de force.',
+      },
+      {
+        promote: false,
+        image: require(`@/assets/images/surf.jpeg`),
+        category: 'Welness',
+        publication: 'Publiée le 6 janvier 2022 par Jeremy Tellier',
+        paragraph: "Rétention d'eau ? La solution, boire de l'eau !",
+        description:
+          "L'importance de donner à son organisme la quantité d'eau suffisante\n" +
+          "            au quotidien, afin d'évitez la rétention d'eau.",
+      },
+      {
+        promote: false,
+        image: require(`@/assets/images/sport.jpeg`),
+        category: 'Sport',
+        publication: 'Publiée le 6 janvier 2022 par Jeremy Tellier',
+        paragraph: 'Je veux être fort !',
+        description:
+          'Article sur mon point de vue concernant le gain de force.',
+      },
+      {
+        promote: false,
+        image: require(`@/assets/images/surf.jpeg`),
+        category: 'Welness',
+        publication: 'Publiée le 6 janvier 2022 par Jeremy Tellier',
+        paragraph: "Rétention d'eau ? La solution, boire de l'eau !",
+        description:
+          "L'importance de donner à son organisme la quantité d'eau suffisante\n" +
+          "            au quotidien, afin d'évitez la rétention d'eau.",
+      },
+    ],
+  }),
+
+  computed: {
+    promotedBlogPost() {
+      return this.blogs.find((blog) => blog.promote === true)
+    },
+
+    promotedBlogPostFalse() {
+      return this.blogs.filter((blog) => blog.promote === false)
+    },
+  },
 }
 </script>
---
+++ src/components/VBlogCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <article>
+    <section class="pl-10">
+      <img
+        class="object-cover h-full w-full"
+        style="height: 300px"
+        :src="image"
+        alt="Sport"
+      />
+
+      <div class="flex justify-between py-3 text-xs">
+        <p class="text-black bg-white p-2 rounded-full px-4 text-center">
+          {{ category }}
+        </p>
+        <p
+          v-if="promote"
+          class="text-black bg-white p-2 mr-20 rounded-full px-4 text-center"
+        >
+          Mise en avant
+        </p>
+        <p class="text-gray-400">
+          {{ publication }}
+        </p>
+      </div>
+
+      <div>
+        <p class="text-xl font-medium">{{ paragraph }}</p>
+        <p>{{ description }}</p>
+      </div>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'VBlogCard',
+
+  props: {
+    promote: {
+      type: Boolean,
+      default: false,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    publication: {
+      type: String,
+      required: true,
+    },
+
+    paragraph: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
